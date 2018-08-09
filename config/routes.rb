175d5f3402ecb0e689c27c811526dd09a10522f4
Spencer Norman <spencerdnorman@gmail.com>
2018-08-08 23:57:34 -0500

    added root path

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
       root to: "sermons#index"
     end
+
+  root to: "sermons#index"
   resources :sermon_sections
   resources :sermons
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
