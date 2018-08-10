8af4025070a95c52c7fc79b79aa18ffcb22b831b
Spencer Norman <spencerdnorman@gmail.com>
2018-08-10 15:57:20 -0500

    added some mailgun config stuff to prod and development

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,9 +92,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = {
-    api_key: 'api-myapikey',
-    domain: 'mydomain.com',
-  }
+  # config.action_mailer.delivery_method = :mailgun
+  # config.action_mailer.mailgun_settings = {
+  #   api_key: 'api-myapikey',
+  #   domain: 'mydomain.com',
+  # }
 end
